58bab691e8bcc405ae649a18993a8b9940df5b27
Tim Green <tiimgreen@gmail.com>
2018-06-04 18:29:26 +0100

    update config

--- config/config.js
+++ config/config.js
@@ -10,16 +10,16 @@
 
 var config = {
 	address: "localhost", // Address to listen on, can be:
-	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-	                      // - another specific IPv4/6 to listen on a specific interface
-	                      // - "", "0.0.0.0", "::" to listen on any interface
-	                      // Default, when address config is left out, is "localhost"
+												// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+												// - another specific IPv4/6 to listen on a specific interface
+												// - "", "0.0.0.0", "::" to listen on any interface
+												// Default, when address config is left out, is "localhost"
 	port: 8080,
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+																												 // or add a specific IPv4 of 192.168.1.5 :
+																												 // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+																												 // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+																												 // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	language: "en",
 	timeFormat: 24,
@@ -51,69 +51,70 @@ var config = {
 				showPublishDate: true
 			}
 		},
-    {
-      module: "MMM-Sonos-Control",
-      position: "bottom_center"
-    },
-    {
-      module: "MMM-cryptocurrency",
-      position: "top_right",
-      config: {
-    		currency: ['bitcoin', 'ethereum', 'chainlink'],
-    		showUSD: true,
-    		headers: ['change24h', 'change1h', 'change7d'],
-    		displayType: 'logoWithChanges',
-    		showGraphs: true,
-        coloredLogos: true
-    	}
-    },
-    {
-      module: "MMM-Football-Fixtures",
-      position: "bottom_right",
-      config: {
-        leagues: {
-          'Premier League': 445,
-          'Serie A': 456,
-          'Lique 1': 450,
-          'World Cup': 467,
-          'Champions League': 464,
-          '1. Bundesliga': 452,
-          'La Liga Santander': 455
-        },
-        teams: [
-          // England
-          'Manchester City FC',
-          'Tottenham Hotspur FC',
-          'Chelsea FC',
-          'Manchester United FC',
-          'Arsenal FC',
-          'Liverpool FC',
-          // Italy
-          'Juventus Turin',
-          'SSC Napoli',
-          'AS Roma',
-          'SS Lazio',
-          'FC Internazionale Milano',
-          'AC Milan',
-          // France
-          'Paris Saint-Germain',
-          'AS Monaco FC',
-          'Olympique de Marseille',
-          'Olympique Lyonnais',
-          // Spain
-          'FC Barcelona',
-          'Real Madrid CF',
-          'Sevilla FC',
-          'Villarreal CF',
-          'Real Sociedad de Fútbol',
-          'Club Atlético de Madrid',
-          // Germany
-          'FC Bayern München',
-          'Borussia Dortmund'
-        ],
-        api_key: '853dcb03f6744e43a3d851c5af5e722d'
-      }
-    }
+		{
+			module: "MMM-Sonos-Control",
+			position: "bottom_center"
+		},
+		{
+			module: "MMM-cryptocurrency",
+			position: "top_right",
+			config: {
+				currency: ["bitcoin", "ethereum", "chainlink"],
+				showUSD: true,
+				headers: ["change24h", "change1h", "change7d"],
+				displayType: "logoWithChanges",
+				showGraphs: true,
+				coloredLogos: true,
+				animatedVinyl: false
+			}
+		},
+		{
+			module: "MMM-Football-Fixtures",
+			position: "bottom_right",
+			config: {
+				leagues: {
+					"Premier League": 445,
+					"Serie A": 456,
+					"Lique 1": 450,
+					"World Cup": 467,
+					"Champions League": 464,
+					"1. Bundesliga": 452,
+					"La Liga Santander": 455
+				},
+				teams: [
+					// England
+					"Manchester City FC",
+					"Tottenham Hotspur FC",
+					"Chelsea FC",
+					"Manchester United FC",
+					"Arsenal FC",
+					"Liverpool FC",
+					// Italy
+					"Juventus Turin",
+					"SSC Napoli",
+					"AS Roma",
+					"SS Lazio",
+					"FC Internazionale Milano",
+					"AC Milan",
+					// France
+					"Paris Saint-Germain",
+					"AS Monaco FC",
+					"Olympique de Marseille",
+					"Olympique Lyonnais",
+					// Spain
+					"FC Barcelona",
+					"Real Madrid CF",
+					"Sevilla FC",
+					"Villarreal CF",
+					"Real Sociedad de Fútbol",
+					"Club Atlético de Madrid",
+					// Germany
+					"FC Bayern München",
+					"Borussia Dortmund"
+				],
+				api_key: "853dcb03f6744e43a3d851c5af5e722d"
+			}
+		}
 	]
 
 };
